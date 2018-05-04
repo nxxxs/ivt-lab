dd00b4cc90c10335a7914484744216fefc222b85
Peter Toth <tothpeti1996@gmail.com>
2018-05-04 01:49:24 -0700

    switch helyett if használata

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,8 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if (firingMode == FiringMode.SINGLE) {
+      //case SINGLE:
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,13 +74,11 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
 
-      case ALL:
+
+    } else {
         // try to fire both of the torpedo stores
         firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
-
-        break;
     }
 
     return firingSuccess;
